613507977000ca3338e1f73666ec57fd892e0f3f
Young <hogusong1@gmail.com>
2018-05-06 14:15:54 -0700

    add question to the file

--- first-duplicate.py
+++ first-duplicate.py
@@ -1,3 +1,21 @@
+# Note: Write a solution with O(n) time complexity and O(1) additional space complexity,
+# since this is what you would be asked to do during a real interview.
+#
+# Given an array a that contains only numbers in the range from 1 to a.length,
+# find the first duplicate number for which the second occurrence has the minimal index.
+# In other words, if there are more than 1 duplicated numbers, return the number for which
+# the second occurrence has a smaller index than the second occurrence of the other number does.
+# If there are no such elements, return -1.
+#
+# Example
+#
+# For a = [2, 3, 3, 1, 5, 2], the output should be
+# firstDuplicate(a) = 3.
+#
+# There are 2 duplicates: numbers 2 and 3.
+# The second occurrence of 3 has a smaller index than than second occurrence of 2 does,
+# so the answer is 3.
+
 def firstDuplicate2(a):
     answer = -1
     while len(a) > 1:
@@ -20,3 +38,5 @@ def firstDuplicate(a):
         else:
             d[a[i]] = i
     return ans
+
+print(firstDuplicate([2, 3, 4, 3, 5, 2]))
